d5eec5a17a8d0a3783828bf20af9ec5b4200240d
Jonathon Tech <jgt.tech@protonmail.com>
2024-06-25 00:22:37 -0500

    WIP

--- go.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-source $HOME/.zshrc
-
-nvm -v
-
-V=$(gvm listall | grep -E '^   go[0-9]+\.[0-9]+(\.[0-9]+)?$' | sort -V | tail -n 1 | awk '{print $1}'); gvm install $V; gvm use $V --default; unset V
